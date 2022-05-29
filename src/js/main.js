56a2730bc703904f96ce14bc309204cc956b607a
Pawel <ricowy96@gmail.com>
2022-05-29 20:17:49 +0200

    add more animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,30 @@ const nav = document.querySelector(".navbar__container");
 const subMenu = document.querySelector(".navbar__sub-menu");
 const burgerBtn = document.querySelector(".burger-btn");
 const burgerBrnBars = document.querySelector(".burger-btn__box-bars");
-const dropDownMenu = document.querySelector(".navbar__links-dropdown");
+// const dropDownMenu = document.querySelector(".navbar__links-dropdown");
 const dropdownArrow = document.querySelector(".navbar__link-icon");
+const navigationLinks = document.querySelectorAll(".navbar__menu-item");
+const navigationSubLinks = document.querySelectorAll(".navbar__sub-item");
 
 const handleNavigation = () => {
 	burgerBrnBars.classList.toggle("burger-btn__box-bars--active");
 	nav.classList.toggle("navbar__container--active");
-	navigationBox.classList.toggle("navbar__box--active");
+	// navigationBox.classList.toggle("navbar__box--active");
 
-	navigationLinks.forEach(link => {
-		link.addEventListener("click", () => {
-			// navigation.classList.remove("navigation__links--active");
-			// burgerBrnBars.classList.remove("burger-btn__box-bars--active");
-		});
+	// navigationLinks.forEach(link => {
+	// 	link.addEventListener("click", () => {
+
+	// 	});
+	// });
+
+	navigationLinks[1].addEventListener("click", () => {
+		subMenu.classList.toggle("navbar__sub-menu--active");
 	});
 };
 
-const dropMenu = () => {
-	subMenu.classList.toggle("navbar__sub-menu--active");
-};
+// const dropMenu = () => {
+// 	subMenu.classList.toggle("navbar__sub-menu--active");
+// };
 
 burgerBtn.addEventListener("click", handleNavigation);
-dropdownArrow.addEventListener("click", dropMenu);
+// dropdownArrow.addEventListener("click", dropMenu);
