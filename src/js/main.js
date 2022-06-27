3e42f40dbadf579935ea468a43d564b17eb440ad
Pawel <ricowy96@gmail.com>
2022-06-27 19:50:29 +0200

    add faq page

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ const dropdownArrow = document.querySelector(".navbar__link-icon");
 const navigationLinks = document.querySelectorAll(".navbar__menu-item");
 const navigationSubLinks = document.querySelectorAll(".navbar__sub-item");
 
+const questionContainer = document.querySelectorAll(".faq__rectangle");
+
 const handleNavigation = () => {
 	burgerBrnBars.classList.toggle("burger-btn__box-bars--active");
 	nav.classList.toggle("navbar__container--active");
@@ -17,4 +19,19 @@ const handleNavigation = () => {
 	document.body.classList.toggle("active");
 };
 
+const handleAccordion = e => {
+	questionContainer.forEach(question => {
+		question.addEventListener("click", e => {
+			const answer = question.querySelector(".faq__answer");
+			const icon = question.querySelector(".faq__question-icon");
+			const answerText = answer.firstChild.nextElementSibling;
+			answer.classList.toggle("faq__answer--active");
+			answerText.classList.toggle("faq__answer-text--active");
+			icon.classList.toggle("faq__question-icon--active");
+
+			console.log(icon);
+		});
+	});
+};
+handleAccordion();
 burgerBtn.addEventListener("click", handleNavigation);
