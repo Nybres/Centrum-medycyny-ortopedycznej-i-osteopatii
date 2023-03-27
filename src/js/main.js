94630ed51d7a53bd48c705573bc6b91175d37aca
Pawel <pawelserbyn@wp.pl>
2023-03-27 20:59:31 +0200

    zmiany w nawigacji

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,46 @@
-const nav = document.querySelector(".navbar__container");
-const subMenu = document.querySelector(".navbar__sub-menu");
-const burgerBtn = document.querySelector(".burger-btn");
-const burgerBrnBars = document.querySelector(".burger-btn__box-bars");
-const dropdownArrow = document.querySelector(".navbar__link-icon");
-const navigationLinks = document.querySelectorAll(".navbar__menu-item");
-const navigationSubLinks = document.querySelectorAll(".navbar__sub-item");
-
 const questionContainer = document.querySelectorAll(".faq__rectangle");
-
-const handleNavigation = () => {
-	burgerBrnBars.classList.toggle("burger-btn__box-bars--active");
-	nav.classList.toggle("navbar__container--active");
-
-	navigationLinks[1].addEventListener("click", () => {
-		subMenu.classList.toggle("navbar__sub-menu--active");
-	});
-
-	document.body.classList.toggle("active");
-};
-
 const handleAccordion = e => {
-	questionContainer.forEach(question => {
-		question.addEventListener("click", e => {
-			const answer = question.querySelector(".faq__answer");
-			const icon = question.querySelector(".faq__question-icon");
-			const answerText = answer.firstChild.nextElementSibling;
-			answer.classList.toggle("faq__answer--active");
-			answerText.classList.toggle("faq__answer-text--active");
-			icon.classList.toggle("faq__question-icon--active");
+  questionContainer.forEach(question => {
+    question.addEventListener("click", e => {
+      const answer = question.querySelector(".faq__answer");
+      const icon = question.querySelector(".faq__question-icon");
+      const answerText = answer.firstChild.nextElementSibling;
+      answer.classList.toggle("faq__answer--active");
+      answerText.classList.toggle("faq__answer-text--active");
+      icon.classList.toggle("faq__question-icon--active");
 
-			console.log(icon);
-		});
-	});
+      console.log(icon);
+    });
+  });
 };
 handleAccordion();
-burgerBtn.addEventListener("click", handleNavigation);
+
+const navBtn = document.querySelector(".navbar__hamburger");
+const submenuBtn = document.querySelector(".navbar__navigation-link--flex");
+const navigation = document.querySelector(".navbar__navigation");
+const subNavigation = document.querySelector(".navbar__navigation-submenu");
+const submenuBackArrow = document.querySelector(".navbar__submenu-back-arrow");
+const body = document.querySelector("body");
+
+const hideBody = params => {
+  body.classList.toggle("body-hide");
+};
+
+const handleNav = params => {
+  navigation.classList.toggle("navbar__navigation--active");
+  subNavigation.classList.remove("navbar__navigation-submenu--active");
+  hideBody();
+};
+
+const handleSubmenu = params => {
+  subNavigation.classList.toggle("navbar__navigation-submenu--active");
+};
+
+const handleBack = params => {
+  handleSubmenu();
+  hideBody();
+};
+
+navBtn.addEventListener("click", handleNav);
+submenuBtn.addEventListener("click", handleSubmenu);
+submenuBackArrow.addEventListener("click", handleBack);
